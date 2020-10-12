c7bd8e1d968b1a4d8d3733b4a033e49fc617bbc0
Fernando Cesar <cmnando.silva@gmail.com>
2020-10-12 16:37:06 -0300

    Criado Agenda.hpp

--- Agenda.hpp
+++ Agenda.hpp
@@ -1,9 +1,29 @@
 #include "Pessoa.hpp"
 #include "Amigo.hpp"
 #include "Conhecido.hpp"
+#include <vector>
 
-class Agenda{
-    private:
+class Agenda
+{
+private:
+    vector<Pessoa> pessoas;
+    int quantidadeAmigos;
+    int quantidadeConhecidos;
 
-    public:
+public:
+    // construtores
+
+    Agenda();
+    Agenda(int);
+
+    //getters
+
+    int getAmigos();
+    int getConhecidos();
+    vector<Pessoa> getPessoas();
+    Pessoa getPessoa(int);
+    void setPessoa(Pessoa,int);
+    void addInformacoes();
+    void imprimeAniversarios();
+    void imprimeEmail();
 };
